c024c264407a70e9010a0c5b22b3fe0d0ec103a2
hfpp2012 <hfpp2012@gmail.com>
2018-08-02 17:16:19 +0800

    first commit

---
+++ src/components/Example.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Example.scss';
+
+// import './Example.css';
+//
+// import styles from './Example.css';
+
+const Example = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <h1>blue hello</h1>
+      Example
+    </div>
+  );
+};
+
+Example.propTypes = {
+};
+
+export default Example;
---
+++ src/counter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Counter = (props) => {
+  return (
+    <div>Hi, Counter</div>
+  )
+}
+
+ReactDom.render(<Counter />, document.getElementById('root'));
